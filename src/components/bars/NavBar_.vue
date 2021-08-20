c8ec1dda60b1388cf8dfae09bb7400398c44db76
B19031506徐颖 <2362527635@qq.com>
2021-08-20 18:52:31 +0800

    增加管理端导航栏
    增加管理端导航栏，“NavBar_”还需完善

--- src/components/bars/NavBar_.vue
+++ src/components/bars/NavBar_.vue
@@ -1,6 +1,8 @@
 <template>
 <!--  管理端口的导航栏-->
-  <div></div>
+  <div>
+    <el-page-header title="title" content="content"></el-page-header>
+  </div>
 </template>
 
 <script>
--- src/components/forms/AdminPort.vue
+++ src/components/forms/AdminPort.vue
@@ -1,12 +1,33 @@
 <template>
 <!--  管理端口，数据端口是DataPort，也就是原来的FormView-->
 <!--  组件中的adminforms文件夹是用来存放一些可能用到的组件，类比于dataforms-->
-  <div></div>
+  <div>
+    <el-container>
+      <el-aside width="200px">
+<!--        侧边栏-->
+        <SideBar_></SideBar_>
+      </el-aside>
+
+      <el-container>
+        <el-header>
+<!--          头部导航栏-->
+          <NavBar_></NavBar_>
+        </el-header>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import SideBar_ from "../bars/SideBar_";
+import NavBar_ from "../bars/NavBar_";
+
 export default {
-  name: "AdminPort"
+  name: "AdminPort",
+  components: {
+    NavBar_,
+    SideBar_
+  }
 }
 </script>
 
